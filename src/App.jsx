e2aa4e5bfd381b187d6da0de251e33ff15b142a4
azegas <azegaspa@gmail.com>
2024-10-23 10:46:31 +0300

    init

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import Navbar from './Navbar';
+import Home from './Home';
+function App() {
+
+  return (
+    <>
+      <div className="app">
+        <Navbar />
+        <div className="content">
+          <Home />
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App;
